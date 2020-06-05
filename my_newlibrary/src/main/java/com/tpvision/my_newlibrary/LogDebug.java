71c71de1567d8628819fc3de73af8189229d1d28
manojmtpvision <manoj.m@tpv-tech.com>
2020-06-05 13:52:03 +0530

    Test commit

---
+++ my_newlibrary/src/main/java/com/tpvision/my_newlibrary/LogDebug.java
@@ -0,0 +1,11 @@
+package com.tpvision.my_newlibrary;
+
+import android.util.Log;
+
+public class LogDebug {
+    public static final  String TAG="Excellent App";
+    public static void d(String message){
+        Log.d(TAG,message);
+
+    }
+}
